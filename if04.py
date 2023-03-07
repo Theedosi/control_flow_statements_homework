73304e6c04a3aaf83005e4b5508de2663c0774ea
Theedosi <dostonsuyunov18@gmail.com>
2023-03-07 10:24:10 +0500

    4

--- if04.py
+++ if04.py
@@ -8,4 +8,13 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+
+    sumx = 0
+    if a > 0:
+        sumx+=1
+    if b >0 :
+        sumx +=1
+    if c > 0 :
+        sumx +=1
+    return sumx
+print(main(1,2,-3))
